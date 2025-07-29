d8fa8de6c9c353b7c1c0248a655b95529ad46056
Shastryji <shivam1234shastry@gmail.com>
2025-07-29 22:02:01 +0530

    new code snippt

---
+++ codesnippt3.ts
@@ -0,0 +1,12 @@
+function calling(fn:()=>void)
+{
+    setTimeout(fn,1000)
+}
+
+calling(function()
+{
+    console.log(("hi there!"))
+})
+
+//create a funciton that takes another function as input, and run it after 1 second
+
