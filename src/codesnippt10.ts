ca35d6555609d1c0cecde53ae43063041f387f90
Shastryji <shivam1234shastry@gmail.com>
2025-08-08 16:30:48 +0530

    union examplet for ts

---
+++ src/codesnippt10.ts
@@ -0,0 +1,23 @@
+interface Admin1 {
+    name: string,
+    age: number,
+}
+interface User2 {
+    name:string,
+    type:string
+}
+
+let admin11 : Admin1 = {
+    name: "shivam",
+    age: 21
+}
+let user11 : User2 = {
+    name: "shastry",
+    type: "user"
+}
+
+function displayType(displayer : Admin1|User2):string{
+    return displayer.name // we can't do displayer.age or displayer.type
+}
+console.log(displayType(admin11)) //shivam
+console.log(displayType(user11)) //shastry
