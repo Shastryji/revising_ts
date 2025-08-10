3bbf48277ba5f3f418efcc287d9c3f22bc60fd7a
Shastryji <shivam1234shastry@gmail.com>
2025-08-10 22:59:48 +0530

    trying some basic chekcout this one1

--- src/codesnippt12.ts
+++ src/codesnippt12.ts
@@ -3,8 +3,28 @@ interface denim  {
     sex: string
 }
 
-type demin {
+type demin = {
  name:string,
  age: number
 }
 
+const came  = {
+    name : "shivam",
+    sex: "karado",
+    logo :"bamboo" // we can add multiple fields any things in this as you can see
+}
+
+const came1 : denim = {
+    name : "shivam",
+    sex: "karado",
+    logo :"bamboo" // we cannot any things in this as you can see idk why we need to find
+}
+
+let demi: demin={
+    name: "shastry",
+    age: 14,
+    wow: "why you doing this"
+}
+
+const p3: denim = came 
+const p2: demin = came // this can't be done with type
